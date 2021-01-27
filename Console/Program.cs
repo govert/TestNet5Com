1293c8c0be06ac297cc51fb6ad8a92269f357e1f
Govert van Drimmelen <govert@icon.co.za>
2021-01-27 21:13:12 +0200

    Add project files.

---
+++ Console/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Microsoft.Office.Interop.Excel;
+
+namespace TestNet5Com
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            DoMyExcelStuffAndCleanup();
+            Console.WriteLine("All Done");
+        }
+        static void DoMyExcelStuffAndCleanup()
+        {
+            DoMyExcelStuff();
+
+            // Now let the GC clean up (repeat, until no more)
+            do
+            {
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+            }
+            while (Marshal.AreComObjectsAvailableForCleanup());
+        }
+
+        static void DoMyExcelStuff()
+        {
+            Application app = new Application();
+            app.Visible = true;
+            var wb = app.Workbooks.Add();
+            var ws = wb.Worksheets[1] as Worksheet;
+            ws.Range["A1"].Value = "Hello";
+            ws.Range["A2"].Formula = "=2+2";
+            ws.Calculate();
+            var result = ws.Range["A2"].Formula;
+            wb.Close(false);
+            Console.WriteLine($"Result: {result}");
+            app.Quit();
+        }
+    }
+
+
+}
---
+++ WinForms/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace TestNet5ComWinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DoMyExcelStuff();
+            GarbageCleanup();
+        }
+
+        private void DoMyExcelStuff()
+        {
+            Excel.Application excelApplication = new Excel.Application();
+            excelApplication.Visible = true;
+            Excel.Workbooks books = excelApplication.Workbooks;
+            Excel.Workbook wBook = books.Add("");
+            Excel.Worksheet wSheet = (Excel.Worksheet)wBook.ActiveSheet;
+
+            Excel.Styles styles = wBook.Styles;
+            Excel.Style columnHeader = styles.Add("ColumnHeader");
+            columnHeader.Font.Size = 12;
+            columnHeader.Font.Bold = true;
+            excelApplication.Range["A1"].Value = "Name";
+            excelApplication.Range["A1"].Style = columnHeader;
+            wBook.SaveAs(@"c:\Temp\tst" + DateTime.Now.ToString("mmss") + ".xlsx");
+
+            // No need for Marshal.ReleaseComObject(...)
+            // No need for ... = null
+            excelApplication.Quit();
+        }
+
+        private void GarbageCleanup()
+        {
+            // Now let the GC clean up (repeat, until no more)
+            do
+            {
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+            }
+            while (Marshal.AreComObjectsAvailableForCleanup());
+        }
+    }
+}
